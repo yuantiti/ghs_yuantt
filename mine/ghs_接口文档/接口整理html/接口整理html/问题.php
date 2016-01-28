e0cdf7391565d0275d812f6ee40e967cb49c4d08
袁婷婷 <yuantingting@readtv.cn>
2016-01-28 13:39:31 +0800

    提交所有的文件

---
+++ mine/ghs_接口文档/接口整理html/接口整理html/问题.php
@@ -0,0 +1,20 @@
+10发送验证码   code=0，其实发送成功了
+11注册激活     code=0
+12用户登录     跳转过去空白页
+14用户信息更新  Fatal error: Call to undefined method UserController::updateAction() in /usr/local/nginx/html/huanqiu/public/index.php on line 200 
+21首页直播商品  code=0
+22首页直播商品  code=0
+22团购接口      code=0
+40商品分类      空白页
+52订单创建      Fatal error: Call to undefined method App2Controller::ordercreate.htmlAction() in /usr/local/nginx/html/huanqiu/public/index.php on line 200 
+53取消订单      Fatal error: Call to undefined method App2Controller::ordercancel.htmlAction() in /usr/local/nginx/html/huanqiu/public/index.php on line 200 
+60版本更新      Fatal error: Call to undefined method AppController::version.htmlAction() in /usr/local/nginx/html/huanqiu/public/index.php on line 200 
+61购物车新增商品   code=0，可能是传的参数不对
+62购物车商品数量修改   code=0，可能是传的参数不对
+72收件地址删除   Fatal error: Can't use method return value in write context in /usr/local/nginx/html/huanqiu/controllers/www/AppHomeController.php on line 982 
+90支付接口       Fatal error: Call to undefined method App2Controller::payInfo.htmlAction() in /usr/local/nginx/html/huanqiu/public/index.php on line 200
+A0版本更新       同上
+A1专题详情       同上
+C1用户账户查询   同上
+C2礼金券校验     同上
+Web专题技术规范  空白页
